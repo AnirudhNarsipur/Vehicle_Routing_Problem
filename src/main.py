da9d2b1cd4055a48531bb45bfb4ca57638266695
Anirudh Narsipur <anirudh.narsipur@gmail.com>
2022-04-18 17:15:14 -0400

    blah

--- src/main.py
+++ src/main.py
@@ -55,26 +55,14 @@ def main(args):
 
 def solve(vars):
     model = CpoModel()
-    truck_routes = []
-    for _ in range(0,vars.vehicles):
-        truck_routes.append(
-            integer_var_list(vars.customers+2,-1,vars.customers)
+    customer_assigs = []
+    for c in range(0,vars.customers):
+        customer_assigs.append(
+          [integer_var(1,vars.)]
         )
-    truck_routes = np.array(truck_routes)
-    #always start at depot
-    for i in range(0,len(truck_routes)):
-        model.add(equals(
-            truck_routes[i,0],0
-        ))
-    # each customer occurs exactly once
-    for c in range(1,vars.customers+1):
-        model.add(equal(count(truck_routes.ravel().tolist(),c), 1))
-    # total served by each truck <= capacity
-    for i in range(0,len(truck_routes)):
-        acc = 0
-        for j in range(0,truck_routes.shape[1]):
-            model.add(if_then(truck_routes[i,j] > 0,sum(acc,vars.demand[truck_routes[i,j]])))
-        model.add(acc <= vars.capacity)
+    customer_assigs = np.array(customer_assigs)
+    #Ordering is unique
+
 
 
 
